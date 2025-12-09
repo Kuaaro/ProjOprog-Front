df32aa22bd38b7439bfc508b3342e8d8fe1fe2e9
namlev <mikipatkowski@protonmail.com>
2025-12-09 20:55:59 +0100

    Rename DTO, add docs

--- proj_oprog_front/lib/catalog/business_logic/icatalog.dart
+++ proj_oprog_front/lib/catalog/business_logic/icatalog.dart
@@ -2,6 +2,6 @@ import 'package:proj_oprog_front/catalog/dto/show_catalog_dto.dart';
 import 'package:proj_oprog_front/catalog/dto/catalog_dto.dart';
 
 abstract class ICatalog {
-  Future<ShowCatalogDto> getCatalogChildren(int id);
+  Future<GetCatalogChildrenResponse> getCatalogChildren(int id);
   Future<void> createCatalog(CatalogDto dto);
 }
